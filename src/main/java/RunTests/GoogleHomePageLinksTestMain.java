f315f7cd80db1fe98543b031cade9e142e2fbe53
ICTER Guru <ictergiru@gmail.com>
2024-08-27 13:50:05 -0400

    Two files moved

--- src/main/java/RunTests/GoogleHomePageLinksTestMain.java
+++ src/main/java/RunTests/GoogleHomePageLinksTestMain.java
@@ -1,11 +1,11 @@
-package step_definitions;
+package RunTests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-public class IRSHomePageLinksTestMain {
+public class GoogleHomePageLinksTestMain {
 
 	//private WebDriver driver;
 
@@ -16,24 +16,24 @@ public class IRSHomePageLinksTestMain {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("https://irs.gov");
+		driver.get("https://google.com");
 		String title = driver.getTitle();
 		System.out.println("Title of the web page is: " + title);
 		String url = driver.getCurrentUrl();
 		System.out.println("URL of the page is: " + url);
 		Thread.sleep(5000); // The open page will be closed in 5 seconds
 
-		driver.findElement(By.id("accessible-megamenu-nav-item-0")).click();
-		if (driver.getPageSource().contains("Filing information for"))
-			System.out.println("The File menu link works");
+		driver.findElement(By.linkText("About")).click();
+		if (driver.getPageSource().contains("Our mission is to"))
+			System.out.println("The About page works");
 		else
-			System.out.println("The File menu link does NOT work");
-		//	Assert.assertEquals(true, driver.getPageSource().contains("Filing information for"));
+			System.out.println("The About page does NOT work");
+		//	Assert.assertEquals(true, driver.getPageSource().contains("Our mission is to"));
 		Thread.sleep(2000);
 
 
-		//		((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
-		//		Thread.sleep(2000);   // temporary wait	   
+		((JavascriptExecutor) driver).executeScript("window.scrollTo(0,document.body.scrollHeight)");
+		Thread.sleep(2000);   // temporary wait	   
 
 
 		driver.quit();// This will close all associated window opened by Selenium
